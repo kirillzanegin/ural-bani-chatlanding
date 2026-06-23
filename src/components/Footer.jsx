135be9f84a2cd6e5cd164f9c188dd371c4a75757
kirillzanegin <kirill.zanegin@gmail.com>
2026-06-23 16:01:59 +0500

    Add legal links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,9 @@ export default function Footer() {
       <div className="container footer-inner">
         <span>Бани под ключ от 15 м²</span>
         <span>Строим по всему Уралу</span>
+        <a href="#privacy">Политика обработки персональных данных</a>
+        <a href="#personal-data-consent">Согласие на обработку персональных данных</a>
+        <a href="#cookies">Cookies</a>
         <a href="#top">Наверх</a>
       </div>
     </footer>
