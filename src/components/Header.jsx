3c2cc10b84328265ef6ff9e8c7ab72023f44c49c
kirillzanegin <kirill.zanegin@gmail.com>
2026-06-28 01:14:27 +0500

    Use root anchors in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,19 @@ export default function Header({ legal = false }) {
   return (
     <header className={legal ? 'header header-legal' : 'header'}>
       <div className="header-brand">
-        <a className="logo" href="./">Бани Урала</a>
+        <a className="logo" href="/">Бани Урала</a>
         {legal && (
-          <a className="legal-return" href="./">Вернуться на основную страницу</a>
+          <a className="legal-return" href="/">Вернуться на основную страницу</a>
         )}
       </div>
       <nav className="nav">
-        <a href="#formats">Форматы</a>
-        <a href="#layouts">Планировки</a>
-        <a href="#cost">Стоимость</a>
-        <a href="#knowledge">База знаний</a>
-        <a href="#faq">FAQ</a>
+        <a href="/#formats">Форматы</a>
+        <a href="/#layouts">Планировки</a>
+        <a href="/#cost">Стоимость</a>
+        <a href="/#knowledge">База знаний</a>
+        <a href="/#faq">FAQ</a>
       </nav>
-      <a className="button button-small" href="#feedback">Рассчитать</a>
+      <a className="button button-small" href="/#feedback">Рассчитать</a>
     </header>
   )
 }
